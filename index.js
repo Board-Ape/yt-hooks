17c5152038207908b3fafa354539bcfa7a81fe3b
Sam Singer <samuel.singer1@gmail.com>
2019-05-07 11:44:33 -0700

    Navigation bar

--- index.js
+++ index.js
@@ -1,15 +1,62 @@
 import React, {Fragment} from 'react';
 import ReactDOM from 'react-dom';
+import './main.css'
 // Parcel is nice for smaller scale bundling instead of Babel
 
 // Create the App
 
 const App = () => {
+    const linkImageStyle = {
+        backgroundImage: 'url(\'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png\')'
+    }
+
     return(
         <Fragment>
-            <nav>
-                <a><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></a>
+            <nav className="navigation">
+                <a><img height="40px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/></a>
+                <ul>
+                    <li>HOME</li>
+                </ul>
             </nav>
+            <main>
+                <div className="leftContent">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png"/>
+                    <form>
+                        <h2 className="formTitle">Add a bookmark</h2>
+                        <div>
+                            <label 
+                                htmlFor="linkTitle" 
+                                className="formLabel">Enter Bookmark Name </label>
+                            <input 
+                                type="text"
+                                name="linkTitle"
+                                minLength="1"
+                                maxLength="25"
+                                placeholder="25 characters max"
+                            />
+                        </div>
+                        <div>
+                            <label 
+                                htmlFor="linkHref" 
+                                className="formLabel">Enter Bookmark Name </label>
+                            <input 
+                                type="text"
+                                name="linkHref"
+                                minLength="7"
+                                placeholder="https://example.com"
+                            />
+                        </div>
+                        <button>Add</button>
+                    </form>
+                </div>
+
+                <div className="rightContent">
+                    <div>
+                        <div style={linkImageStyle}/>
+                        <div><h2><a href="#">My link!</a></h2></div>
+                    </div>
+                </div>
+            </main>
         </Fragment>
     )
 }
